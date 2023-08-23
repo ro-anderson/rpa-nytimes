c828c6e7425006adaf1b76c4987475a74af8da90
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2023-08-23 13:32:27 -0300

    feat: my second tutorial robot

--- tasks.py
+++ tasks.py
@@ -1,6 +1,22 @@
 from robocorp.tasks import task
+from robocorp import browser
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def robot_spare_bin_python():
+    """Insert the sales data for the week and export it as a PDF"""
+    browser.configure(
+        slowmo=100,
+    )
+    open_the_intranet_website()
+    log_in()
+
+def open_the_intranet_website():
+    """Navigates to the given URL"""
+    browser.goto("https://robotsparebinindustries.com/")
+
+def log_in():
+    """Fills in the login form and clicks the 'Log in' button"""
+    page = browser.page()
+    page.fill("#username", "maria")
+    page.fill("#password", "thoushallnotpass")
+    page.click("button:text('Log in')")
