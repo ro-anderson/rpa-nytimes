a1e10f027dde5e43043489453663732e21a6dfe7
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2023-08-23 15:38:21 -0300

    feat(test): add tasks definition

--- tasks.py
+++ tasks.py
@@ -1,24 +1,32 @@
-from robocorp.tasks import task
-from robocorp import browser
+from RPA.Browser.Selenium import Selenium
 
-@task
-def robot_spare_bin_python():
-    """Insert the sales data for the week and export it as a PDF"""
-    browser.configure(
-        slowmo=100,
-    )
-    open_the_intranet_website()
-    log_in()
+browser_lib = Selenium()
 
-@task
-def open_the_intranet_website():
-    """Navigates to the given URL"""
-    browser.goto("https://robotsparebinindustries.com/")
 
-@task
-def log_in():
-    """Fills in the login form and clicks the 'Log in' button"""
-    page = browser.page()
-    page.fill("#username", "maria")
-    page.fill("#password", "thoushallnotpass")
-    page.click("button:text('Log in')")
+def open_the_website(url):
+    browser_lib.open_available_browser(url)
+
+
+def search_for(term):
+    input_field = "css:input"
+    browser_lib.input_text(input_field, term)
+    browser_lib.press_keys(input_field, "ENTER")
+
+
+def store_screenshot(filename):
+    browser_lib.screenshot(filename=filename)
+
+
+# Define a main() function that calls the other functions in order:
+def main():
+    try:
+        open_the_website("https://robocorp.com/docs/")
+        search_for("python")
+        store_screenshot("output/screenshot.png")
+    finally:
+        browser_lib.close_all_browsers()
+
+
+# Call the main() function, checking that we are running as a stand-alone script:
+if __name__ == "__main__":
+    main()
