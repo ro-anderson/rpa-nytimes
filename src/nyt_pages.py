db68c36ae3f490f6e83c7ac853a818738253c7ce
Rodrigo Anderson <rodrigo.anderson@emcasa.com>
2023-08-30 12:41:23 -0300

    feat: apply ErrorHandlingContext; add docstrings; clean code

--- src/nyt_pages.py
+++ src/nyt_pages.py
@@ -3,44 +3,45 @@ import re
 import datetime
 from time import sleep
 import logging
-from src.utility import Utility, ErrorHandler
+from src.utility import Utility, ErrorHandler, ErrorHandlingContext
 from src.excel_handler import ExcelHandler
 import json
 
 # Page Object Pattern Implementation
 # Redefining the NYTBasePage class for context
 class NYTBasePage:
-    """Base class representing common functionalities for all NYT pages."""
+    '''Base class representing common functionalities for all NYT pages.'''
     def __init__(self, browser):
         self.browser = browser
 
     @ErrorHandler.handle_errors("Error when opening the website.")
     def open_the_website(self, url):
-        """
-        Open the specified website URL in the browser.
-        
+        '''Open the specified website URL in the browser.
+
         Args:
-        - url (str): The website URL to open.
-        
-        Returns:
-        - None
-        """
+        - url (): Description needed.
+
+            Args:
+            - url (str): The website URL to open.
+
+            Returns:
+            - None
+            '''
 
         self.browser.open_available_browser(url)
 
 class HomePage(NYTBasePage):
-    """Represents the main page of the New York Times."""
+    '''Represents the main page of the New York Times.'''
 
     def __init__(self, browser):
         super().__init__(browser)
         self.url = "https://www.nytimes.com/"
 
 class SearchResultsPage(NYTBasePage):
-    """Represents the search results page on the New York Times."""
+    '''Represents the search results page on the New York Times.'''
     def __init__(self, browser):
         super().__init__(browser)
         self.utils = Utility(browser)
-
         self.wi = WorkItems()
         self.wi.get_input_work_item()
         self.input_wi = self.wi.get_work_item_variables() 
@@ -48,7 +49,6 @@ class SearchResultsPage(NYTBasePage):
         self.news_categories = self.input_wi["news_categories"]
         self.number_of_months = self.input_wi["number_of_months"]
         self.excel_handler = ExcelHandler()
-        
         self.search_dates_range = self.utils.calculate_search_dates_range(self.number_of_months)
         self.config = self.load_config()
 
@@ -75,79 +75,104 @@ class SearchResultsPage(NYTBasePage):
         self.directory_output_path = self.config["output_directory"]
 
     def load_config(self, filename="config.json"):
+        '''load_config function.
+
+        Args:
+        - filename (): Description needed.
+        '''
         with open(filename, 'r') as file:
             config = json.load(file)
         return config
 
     def filter_articles_by_search_dates_range(self, articles):
-        """
-        Filters articles based on a given date range.
-        
+        '''Filters articles based on a given date range.
+
         Args:
-        - articles (list): List of article dictionaries.
-        - search_dates_range (dict): Dictionary with 'start_date' and 'end_date' keys.
-        
-        Returns:
-        - list: Filtered list of articles.
-        """
-        
+        - articles (): Description needed.
+
+            Args:
+            - articles (list): List of article dictionaries.
+            - search_dates_range (dict): Dictionary with 'start_date' and 'end_date' keys.
+
+            Returns:
+            - list: Filtered list of articles.
+            '''
+
         # Convert the start_date and end_date strings to datetime objects
         start_date = datetime.datetime.strptime(self.search_dates_range['start_date'], '%m/%d/%Y')
         end_date = datetime.datetime.strptime(self.search_dates_range['end_date'], '%m/%d/%Y')
-        
+
         # Filter the articles based on the date range
         filtered_articles = [
             article for article in articles
             if start_date <= datetime.datetime.strptime(self.utils.text_to_formatted_date(article['date']), '%m/%d/%Y') <= end_date
         ]
-        
+
         return filtered_articles
 
     def count_search_phrases(self, title: str, description: str, term: str) -> int:
-        """
-        Counts the occurrences of the search phrase (term) in the title and description.
-        
+        '''Counts the occurrences of the search phrase (term) in the title and description.
+
         Args:
-        - title (str): The article title.
-        - description (str): The article description.
-        - term (str): The search phrase.
-        
-        Returns:
-        - int: The total count of the search phrase in the title and description.
-        """
-        
+        - title (): Description needed.
+        - description (): Description needed.
+        - term (): Description needed.
+
+            Args:
+            - title (str): The article title.
+            - description (str): The article description.
+            - term (str): The search phrase.
+
+            Returns:
+            - int: The total count of the search phrase in the title and description.
+            '''
+
         # Counting occurrences of term in both title and description
         count_in_title = title.lower().count(term.lower())
         count_in_description = description.lower().count(term.lower())
-        
+
         return count_in_title + count_in_description
 
     def get_available_categories(self):
-        """Extracts all available categories from the dropdown list."""
-        
+        '''Extracts all available categories from the dropdown list.
+        Args:
+
+            categories = [element.text for element in self.browser.get_webelements(self.xpath_categories)]
+
+            # Remove numbers from each category name
+            cleaned_categories = [re.sub(r'\d+', '', category) for category in categories]
+
+            return cleaned_categories
+        '''
         categories = [element.text for element in self.browser.get_webelements(self.xpath_categories)]
 
         # Remove numbers from each category name
         cleaned_categories = [re.sub(r'\d+', '', category) for category in categories]
-        
+
         return cleaned_categories
 
     def get_valid_categories(self):
-        """Checks if the provided categories are part of the available categories."""
-        available_categories = self.get_available_categories()
-        
+        '''Checks if the provided categories are part of the available categories.
+        Args:
+            '''
+
         # Return only those categories from news_categories that exist in the available_categories
+        available_categories = self.get_available_categories()
         valid_categories = [category for category in self.news_categories if category in available_categories]
-        
+
         # Save output workitem - versioning valid categories
         self.wi.create_output_work_item({"valid_categories":valid_categories}, save=True)
 
         return valid_categories
 
     def check_categories(self):
+        '''check_categories function.
+
+        Args:
+        '''
+
         # Step 0: Filter out categories that are not present on the webpage
         valid_categories = self.get_valid_categories()    
-
         # If the resulting list is empty, only select 'Any'
         if not valid_categories:
             self.check_category("Any")
@@ -166,30 +191,27 @@ class SearchResultsPage(NYTBasePage):
             return
 
     def check_category(self, news_category):
+        '''check_category function.
+
+        Args:
+        - news_category (): Description needed.
+        '''
 
         # XPath to locate the checkbox based on the inner text of its corresponding label
         checkbox_xpath = f'//label[contains(., "{news_category}")]/input[@type="checkbox"]'
-        
+
         # Check if the checkbox is already checked
         is_checked = self.browser.is_element_attribute_equal_to(checkbox_xpath, "checked", "true")
-        
+
         # If the checkbox is not already checked, click on it
         if not is_checked:
             self.browser.click_element_when_clickable(checkbox_xpath)
 
-
-    def close_modals_updated(self):
-        """Close modals that might appear during page interactions."""
-        try:
-            # Wait for and click the terms update acceptance button (if it appears)
-            self.browser.click_element_when_clickable(self.terms_update_acceptance_selector, timeout=15)
-
-        except Exception as e:  # Catch all exceptions to be safe, but you can specify the exact exception type if desired
-            # If the modal doesn't appear within the timeout, just log a message and continue
-            print(f"Modal did not appear or could not be closed: {e}")
-
     def close_modals(self):
-        """Close modals that might appear during page interactions."""
+        '''Close modals that might appear during page interactions.
+        Args:
+        '''
+
         try:
             # Wait for and click the terms update acceptance button (if it appears)
             self.browser.click_element_when_clickable(self.terms_update_acceptance_selector, timeout=15)
@@ -205,103 +227,94 @@ class SearchResultsPage(NYTBasePage):
             print(f"Cookies acceptance modal did not appear or could not be closed: {e}")
 
     def apply_filters(self):
-        """
-        Apply filters on the search results page to narrow down the articles based on criteria.
-        
+        '''Apply filters on the search results page to narrow down the articles based on criteria.
+
         Returns:
         - None
-        """
-        # Placeholder for the actual filter application logic from tasks.py
+        Args:
+        '''
 
-        # Close any modals that appear
         self.close_modals()
 
-        # Click on magnifier button
-        self.browser.click_element_when_clickable(self.magnifier_button, timeout=10)
+        with ErrorHandlingContext("Error when clicking on magnifier button"):
+            self.browser.click_element_when_clickable(self.magnifier_button, timeout=10)
 
-        # Input the term into the search field using the browser
-        self.browser.input_text_when_element_is_visible(self.search_input_selector, self.search_phrase)
+        with ErrorHandlingContext("Error when inputting the search term"):
+            self.browser.input_text_when_element_is_visible(self.search_input_selector, self.search_phrase)
 
-        # Click on magnifier button
-        self.browser.click_element_when_clickable(self.go_button, timeout=10)
+        with ErrorHandlingContext("Error when clicking on magnifier button"):
+            self.browser.click_element_when_clickable(self.go_button, timeout=10)
 
-        # Click on multiselect date range button button to Collapse
-        self.browser.click_element_when_clickable(self.multiselect_date_range_button, timeout=10)
+        with ErrorHandlingContext("Error when selecting date range"):
+            self.browser.click_element_when_clickable(self.multiselect_date_range_button, timeout=10)
+            self.browser.click_element_when_clickable(self.specific_dates_button, timeout=10)
+            self.browser.input_text_when_element_is_visible(self.start_date_input_button, self.search_dates_range["start_date"])
+            self.browser.input_text_when_element_is_visible(self.end_date_input_button, self.search_dates_range["end_date"])
+            self.browser.press_keys(self.end_date_input_button , "ENTER")
 
-        # Click on specific dates
-        self.browser.click_element_when_clickable(self.specific_dates_button, timeout=10)
+        with ErrorHandlingContext("Error when clicking on magnifier button"):
+            self.browser.click_element_when_clickable(self.multiselect_button, timeout=10)
 
-        # Input the start_date 
-        self.browser.input_text_when_element_is_visible(self.start_date_input_button, self.search_dates_range["start_date"])
+        with ErrorHandlingContext("Error when checking categories"):
+            self.check_categories()
 
-        # Input date the end_date 
-        self.browser.input_text_when_element_is_visible(self.end_date_input_button, self.search_dates_range["end_date"])
-
-        # Click Enter
-        self.browser.press_keys(self.end_date_input_button , "ENTER")
-    
-        # Click on magnifier button
-        self.browser.click_element_when_clickable(self.multiselect_button, timeout=10)
-
-        # Check checkbox category
-        self.check_categories()
-
-        # Click on magnifier button to Collapse
-        self.browser.click_element_when_clickable(self.multiselect_button, timeout=10)
-
- 
+        with ErrorHandlingContext("Error when collapsing magnifier button"):
+            self.browser.click_element_when_clickable(self.multiselect_button, timeout=10)
 
     def extract_articles(self):
-        """
-        Extract article details like title, date, and description from the search results page.
-        
+        '''Extract article details like title, date, and description from the search results page.
+
         Returns:
         - list: A list of dictionaries containing article details.
-        """
-        # Placeholder for the actual article extraction logic from tasks.py
+        '''
+
         while True:
             try:
                 # Wait until the "SHOW MORE" button is clickable and click on it
                 self.browser.click_element_when_clickable(show_more_button, timeout=2)
-
             except:
                 # If the "SHOW MORE" button is not found or not clickable within the timeout period, break out of the loop
                 break
 
-        # get data
         articles_data = []
 
-        # Locate all root div elements for articles based on a structure commonality.
-        article_divs = self.browser.find_elements(self.root_div_elements_css)
+        with ErrorHandlingContext("Error when locating article div elements"):
+            # Locate all root div elements for articles based on a structure commonality.
+            article_divs = self.browser.find_elements(self.root_div_elements_css)
 
         for div in article_divs:
+            with ErrorHandlingContext("Error when extracting article details"):
+                # Use the helper function
+                date = self.utils.text_to_formatted_date(self.utils.safe_get_text(f"css:.css-{self.date_locator}", parent=div))
 
-            # Use the helper function
-            date = self.utils.text_to_formatted_date(self.utils.safe_get_text(f"css:.css-{self.date_locator}", parent=div))
+                # If date is out of the range, skip the current iteration
+                if not self.utils.is_date_in_range(date, self.search_dates_range):
+                    continue
 
-            # If date is out of the range, skip the current iteration
-            if not self.utils.is_date_in_range(date, self.search_dates_range):
-                continue
+                title = self.utils.safe_get_text(f"css:.css-{self.title_locator}", parent=div)
+                description = self.utils.safe_get_text(f"css:.css-{self.description_locator}", parent=div)
+                image_url = self.utils.safe_get_image_url(f"css:.css-{self.image_locator}", parent=div)
 
-            title = self.utils.safe_get_text(f"css:.css-{self.title_locator}", parent=div)
-            description = self.utils.safe_get_text(f"css:.css-{self.description_locator}", parent=div)
-            image_url = self.utils.safe_get_image_url(f"css:.css-{self.image_locator}", parent=div)
+                # download image by the url
+                print(f"\nDownloading image with: date:{date}\ntitle:{title}\n")
+                picture_filename = self.utils.download_image_with_uuid(image_url, self.directory_output_path)
 
-            # download image by the url
-            print(f"\nDownloading image with: date:{date}\ntitle:{title}\n")
-            picture_filename = self.utils.download_image_with_uuid(image_url, self.directory_output_path)
+                # Storing data in a dictionary and appending to the list
+                article_data = {
+                    "date": date,
+                    "title": title,
+                    "description": description,
+                    "picture_filename": picture_filename,
+                    "contains_money_format_on_title_or_description": self.utils.contains_money_format_on_title_or_description(title, description),
+                    "count_search_phrases": self.utils.count_search_phrases(title, description, self.search_phrase)
+                }
+                articles_data.append(article_data)
 
-            # Storing data in a dictionary and appending to the list
-            article_data = {
-                "date": date,
-                "title": title,
-                "description": description,
-                "picture_filename": picture_filename,
-                "contains_money_format_on_title_or_description": self.utils.contains_money_format_on_title_or_description(title, description),
-                "count_search_phrases": self.utils.count_search_phrases(title, description, self.search_phrase)
-            }
-            articles_data.append(article_data)
+        with ErrorHandlingContext("Error when filtering articles by date range and creating Excel"):
+            # As the datetime filter of nytimes is not working as expected, we need to filter the articles for the data range that we want
+            filtred_articles = self.filter_articles_by_search_dates_range(articles_data)
+            self.excel_handler.create_excel(filtred_articles)
 
-        # As the datetime filter of nytimes is not working as expected, we need to filter the articles for the data range that we want
-        filtred_articles = self.filter_articles_by_search_dates_range(articles_data)
-        self.excel_handler.create_excel(filtred_articles)
+            # As the datetime filter of nytimes is not working as expected, we need to filter the articles for the data range that we want
+            filtred_articles = self.filter_articles_by_search_dates_range(articles_data)
+            self.excel_handler.create_excel(filtred_articles)
